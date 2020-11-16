68648c60a4051ede3970e25a35d204d15987e2a5
Dhanush <dhanuraj7258@gmail.com>
2020-11-16 09:46:28 +0530

    navbar with hero added with gatsby and styled components

---
+++ src/data/MenuData.js
@@ -0,0 +1,22 @@
+export const menuData = [
+  {
+    id: 1,
+    title: "About",
+    link: "/about",
+  },
+  {
+    id: 2,
+    title: "Trips",
+    link: "/trips",
+  },
+  {
+    id: 3,
+    title: "Careers",
+    link: "/careers",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "/contact",
+  },
+]
